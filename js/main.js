db26140e2b54af76ea198569c489cabef9a576de
Ivan Diaz <idiaz3@gmu.edu>
2015-02-04 17:13:28 -0500

    Added new files and music

--- js/main.js
+++ js/main.js
@@ -5,97 +5,113 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, cr
 
 function preload() {
 
+game.load.audio('nujabes',['assets/audio/nujabes_afternoon.mp3']);
 game.load.image('sky', 'assets/sky.png');
 game.load.image('ground', 'assets/platform.png');
 game.load.image('star', 'assets/star.png');
+game.load.image('oneUp','assets/firstaid.png');
 game.load.spritesheet('Dog','assets/Dog.png',50,50);
 game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+game.load.spritesheet('enemy','assets/baddie.png',32,48);
 
 }
 
 var player;
+var dogs;
 var platforms;
 var cursors;
-
+var enemies;
+var giveLife;
 var stars;
 var score = 0;
 var scoreText;
 var clock = 60000;
 var clockText;
+var music;
 
 function create() {
 
-	//  We're going to be using physics, so enable the Arcade Physics system
+    music = game.add.audio('nujabes');
+    music.play();
+    music.volume = 0.5;
+
+    //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	//  A simple background for our game
-	game.add.sprite(0, 0, 'sky');
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
 
-	//  The platforms group contains the ground and the 2 ledges we can jump on
-	platforms = game.add.group();
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
 
     //  We will enable physics for any object that is created in this group
-	platforms.enableBody = true;
+    platforms.enableBody = true;
 
     // Here we create the ground.
-	var ground = platforms.create(0, game.world.height - 64, 'ground');
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
 
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-	ground.scale.setTo(2, 2);
+    ground.scale.setTo(2, 2);
 
     //  This stops it from falling away when you jump on it
-	ground.body.immovable = true;
+    ground.body.immovable = true;
 
     //  Now let's create two ledges
-	var ledge = platforms.create(400, 400, 'ground');
-	ledge.body.immovable = true;
+    var ledge = platforms.create(400, 400, 'ground');
+    ledge.body.immovable = true;
 
-	ledge = platforms.create(-150, 250, 'ground');
-	ledge.body.immovable = true;
+    ledge = platforms.create(-150, 250, 'ground');
+    ledge.body.immovable = true;
 
-	// The player and its settings
-	player = game.add.sprite(32, game.world.height - 150, 'dude');
+    // The player and its settings
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
 
-	//  We need to enable physics on the player
-	game.physics.arcade.enable(player);
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(player);
 
-	//  Player physics properties. Give the little guy a slight bounce.
-	player.body.bounce.y = 0.2;
-	player.body.gravity.y = 300;
-	player.body.collideWorldBounds = true;
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 
-	//  Our two animations, walking left and right.
-	player.animations.add('left', [0, 1, 2, 3], 10, true);
-	player.animations.add('right', [5, 6, 7, 8], 10, true);
-    
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
    
-	//Adding dog groups
-
-	dogs = game.add.group(); 
-	dogs.enableBody = true;
-
-	//Adding dog into the game at random X and Y
-	var dog = dogs.create(game.world.randomX, game.world.randomY, 'Dog');
-
-
-	//Adding dog physics
-	//game.physics.arcade.enable(dog);
+    enemies = game.add.group();
+    enemies.enableBody = true;
+    var enemy = enemies.create(game.world.randomX, game.world.randomY, 'enemy');
     
-	dog.body.bounce.y = 0.2;
-	dog.body.gravity.y = 320;
-	dog.body.collideWorldBounds = true;
+    enemy.body.bounce.y = 0;
+    enemy.body.gravity.y = 320;
+ 
+    //Adding dog groups
 
-	//  Finally some stars to collect
-	stars = game.add.group();
+    dogs = game.add.group(); 
+    dogs.enableBody = true;
 
-	//  We will enable physics for any star that is created in this group
-	stars.enableBody = true;
+    //Adding dog into the game at random X and Y
+    var dog = dogs.create(game.world.randomX, game.world.randomY, 'Dog');
 
-	//  Here we'll create 12 of them evenly spaced apart
-	var star = stars.create(game.world.randomX, game.world.randomY, 'star');
-	star.body.gravity.y = 300;
-	star.body.bounce.y = 0.9;
-	game.time.events.repeat(Phaser.Timer.SECOND*30, 50, resurrectStar, this);
+
+    //Adding dog physics
+    //game.physics.arcade.enable(dog);
+    
+    dog.body.bounce.y = 0.2;
+    dog.body.gravity.y = 320;
+    dog.body.collideWorldBounds = true;
+
+    //  Finally some stars to collect
+    stars = game.add.group();
+
+    //  We will enable physics for any star that is created in this group
+    stars.enableBody = true;
+
+    //  Here we'll create 12 of them evenly spaced apart
+    var star = stars.create(game.world.randomX, game.world.randomY, 'star');
+    star.body.gravity.y = 300;
+    star.body.bounce.y = 0.9;
+    game.time.events.repeat(Phaser.Timer.SECOND*30, 50, resurrectStar, this);
     /*for (var i = 0; i < 12; i++)
     {
         //  Create a star inside of the 'stars' group
@@ -164,8 +180,6 @@ function create() {
 
     }
 
-game.state.add('main',mainState);
-game.state.start('main');
 
 function collectDog (player, dogs) {
     
